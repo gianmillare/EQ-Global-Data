ab85785b87883709740d752345dfcdbda7624f7d
gianmillare <gianmillaregsm@gmail.com>
2020-03-21 21:00:42 -0700

    looped through the feature dictionary to append all data in the 'mag' key

--- eq_data.py
+++ eq_data.py
@@ -15,10 +15,24 @@ filename = 'data/eq_1_day.json'
 
 
 
+# #  Part 2: List all the earthquakes
+# with open(filename) as f:
+#     all_eq_data = json.load(f)
+#
+# all_eq_dicts = all_eq_data['features'] # This is how we would normally 'call' a specific dictionary
+# print(len(all_eq_dicts))
 
-#  Part 2: List all the earthquakes
+
+
+# Part 3: Extracting Magnitudes
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-all_eq_dicts = all_eq_data['features'] # This is how we would normally 'call' a specific dictionary
-print(len(all_eq_dicts))
+all_eq_dicts = all_eq_data['features']
+
+mags = [] # Create an open list to store the information
+for eq_dict in  all_eq_dicts: # loop through the all dictionaries under 'features'
+    mag = eq_dict['properties']['mag'] # store all mag data into a variable
+    mags.append(mag) # append the mag data into the empty list
+print(mags[:10])
+
